9eaa7667800dc7d080724c5546e9c569d209ccda
Edison III <garvidaedisoniii@gmail.com>
2024-12-26 18:25:34 +0800

    WebConfig

--- PatientDetails/PatientDetails_DAL/DB/MSSQLConnectionProvider.cs
+++ PatientDetails/PatientDetails_DAL/DB/MSSQLConnectionProvider.cs
@@ -1,29 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using PatientDetails_DAL.DB;
+
 
 namespace PatientDetails_DAL.DB
 {
     public static class MSSQLConnectionProvider
     {
+        private static readonly string connString;
+        public static readonly int connTimeout = 60;
+
+        static MSSQLConnectionProvider()
+        {
+            var connSetting = ConfigurationManager.ConnectionStrings["PatientDetails"];
+            connString = connSetting.ConnectionString;
+        }
+
         public static string GetConnectionString()
         {
-            // SQL Server Details
-            string server = "MSI-EDISONIII\\SQLEXPRESS";
-            string database = "PatientDetails";
-            string user = "asherkairos";
-            string password = "Asher.kairos0922";
-
-            // Build the connection string
-            return $"Server={server};Database={database};User Id={user};Password={password};";
+            return connString;
         }
-        
+
         public static int GetConnectionTimeout()
         {
-            return 60;
+            return connTimeout;
         }
     }
 }
